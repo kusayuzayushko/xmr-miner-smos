add831b80dc68a23a68951632f728455e1d71ef3
cryptoscum <cryptoscum@gmail.com>
2017-08-26 15:20:46 +0700

    Add libmicrohttpd10 installatins

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo mv /root/miner_org/wolf-xmr-miner/ /root/miner_org/wolf-xmr-miner-bkp
 sudo mkdir /root/miner_org/wolf-xmr-miner/
 sudo cp -r * /root/miner_org/wolf-xmr-miner/
 sudo chmod -R 777 /root/miner_org/wolf-xmr-miner/
-
+sudo apt install libmicrohttpd10 -y
 echo "Now you can open Web Innerface, choose wolf-xmr-miner in global options"
 echo "and put something like -s xmr-eu1.nanopool.org:14444 -w YOUR_XMR_WALLET.$rigName -p z -i 1024 "
 echo "into commandline string."
