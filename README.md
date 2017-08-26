cc9fc178bdca2401154fadd350a3c9543b981f63
cryptoscum <cryptoscum@gmail.com>
2017-08-26 13:49:56 +0700

    Ready for testing

---
+++ README.md
@@ -0,0 +1,23 @@
+# XMR miner for SimpleMining OS (AMD)
+
+## Installation
+
+Open the terminal via ssh or directly via connected keyboard/monitor.
+
+Install git
+
+`sudo apt update && sudo apt install git-core`
+
+Clone this repository
+
+`git clone https://github.com/kusayuzayushko/xmr-miner-smos.git`
+
+go into directory
+
+`cd xmr-miner-smos`
+
+run
+
+`./install.sh`
+
+edit your global config.
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo mv /root/miner_org/wolf-xmr-miner/ /root/miner_org/wolf-xmr-miner-bkp
+sudo mkdir /root/miner_org/wolf-xmr-miner/
+sudo cp -r * /root/miner_org/wolf-xmr-miner/
+echo "Now you can open Web Innerface, choose wolf-xmr-miner in global options"
+echo "and put something like -s "xmr-eu1.nanopool.org:14444\ -w YOUR_XMR_WALLET.$rigName -p z -i 1024 "
+echo "into commandline string."
