f4e5688276e9a9b16e76814b5a4cd0b20f8ca5b6
cryptoscum <cryptoscum@gmail.com>
2017-08-26 14:58:34 +0700

    Edit permissions

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 sudo mv /root/miner_org/wolf-xmr-miner/ /root/miner_org/wolf-xmr-miner-bkp
 sudo mkdir /root/miner_org/wolf-xmr-miner/
 sudo cp -r * /root/miner_org/wolf-xmr-miner/
+sudo chmod -R 777 /root/miner_org/wolf-xmr-miner/
+
 echo "Now you can open Web Innerface, choose wolf-xmr-miner in global options"
 echo "and put something like -s xmr-eu1.nanopool.org:14444 -w YOUR_XMR_WALLET.$rigName -p z -i 1024 "
 echo "into commandline string."
